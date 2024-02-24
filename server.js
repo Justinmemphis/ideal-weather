c5230e02790fe328a5a2cfb6063abfb64c36f122
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2024-02-24 11:27:21 -0600

    build basic server logic

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 // Main server file
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+require('dotenv').config();
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '/')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
+
+
+const port = process.env.Port || 3000;
+
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
+});
